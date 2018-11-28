86bdb72a1efe628b846460243493904a7c5ae57e
Cooper Brislain <cbrislain@gmail.com>
2018-11-27 20:37:38 -0800

    initial

---
+++ warm.ino/warm.ino.ino
@@ -0,0 +1,47 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 144
+#define DATA_PIN 7
+#define CLOCK_PIN 6
+
+CRGB leds[NUM_LEDS];
+
+int index;
+int count;
+int color;
+int space;
+
+void fade(){
+    for(int i=0; i<NUM_LEDS; i++){
+       leds[i].fadeToBlackBy( 25 );
+    }
+}
+
+void setup() {
+    // put your setup code here, to run once:
+    FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+    for (int i=0; i<NUM_LEDS; i++) {
+      leds[i] = CRGB::Black;
+    }
+    index=0;
+    count=0;
+}
+
+void warm(int which) {
+    leds[which] += CHSV(color,255,200);
+}
+
+void loop() {
+    if(count%8 == 0){
+        color = random(255);
+        index = random(NUM_LEDS);
+        space = random(7)-4;
+    }
+    warm(index);
+    fade();
+    index = (NUM_LEDS+(index+space))%NUM_LEDS;
+    FastLED.show();
+    count++;
+    delay(175);
+}
+
